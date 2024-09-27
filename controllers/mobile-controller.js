2d7e69b9f17cb7d3c40e197a273dd1a996a80886
bayrem agrebi <beyrem.agrebi@proservices-training-company.com>
2024-09-27 13:42:41 +0100

    clean the code

--- controllers/mobile-controller.js
+++ controllers/mobile-controller.js
@@ -1,10 +1,11 @@
 exports.mobileController = (socketio) => {
   const connectedUsersByFacilites = new Map();
   socketio.on("connection", (userSocket) => {
-    userSocket.on('new-connection-by-facilities', (data) => {
-      const { userId, facilityName, fullName, facilityId } = data;
-      let users;
 
+
+    userSocket.on('new-connection-by-facilities', (data) => {
+      const { userId, fullName, facilityId } = data;
+      let users;
       if (connectedUsersByFacilites.has(facilityId)) {
         users = connectedUsersByFacilites.get(facilityId);
       } else {
@@ -24,14 +25,13 @@ exports.mobileController = (socketio) => {
       }
     });
 
+
     userSocket.on('disconnect-by-facilities', (data) => {
       const { userId, facilityName, fullName, facilityId } = data;
 
       if (connectedUsersByFacilites.has(facilityId)) {
         let users = connectedUsersByFacilites.get(facilityId);
         const userIndex = users.findIndex(user => user.userId === userId);
-        console.log(userIndex);
-
         if (userIndex !== -1) {
           users.splice(userIndex, 1);
           if (users.length > 0) {
@@ -39,21 +39,26 @@ exports.mobileController = (socketio) => {
           } else {
             connectedUsersByFacilites.delete(facilityId);
           }
-
-
           console.log(`${fullName} was disconnected From =>`, { facility: facilityName });
           socketio.in(facilityId).emit("disconnect-user", connectedUsersByFacilites.get(facilityId));
           userSocket.leave(facilityId);
         }
       }
     });
+
+
     userSocket.on("typing-mobile", (room, userId) => {
       socketio.in(room).emit("typing-mobile", { userId, room })
     });
+
+
     userSocket.on("stop-typing-mobile", (room) => { socketio.in(room).emit("stop-typing-mobile") });
+
 
     userSocket.on("send-message-mobile", async (room, message, senderId) => {
       socketio.in(room).emit("message-recieved-mobile", { room, message, senderId })
     });
+
+
   })
 }
